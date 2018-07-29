3c12b9bdc18a259cfe28748991c92157948e3f69
swapsam <swapsam555@gmail.com>
2018-07-29 12:42:22 +0530

    s2 file

---
+++ vehicle_interface.py
@@ -0,0 +1,15 @@
+from truck import truck
+
+truck_obj = truck(100, 60, 40, 1500, 10)
+print(truck_obj.maxload)
+print(truck_obj.speedlimit)
+print(truck_obj.load())
+print(truck_obj.unload())
+print(truck_obj.no.of_wheels)
+print(truck_obj.speed)
+print(truck_obj.weight)
+print(truck_obj.mileage)
+print(truck_obj.go-forward())
+print(truck_obj.reverse())
+print(truck_obj.stop)
+
